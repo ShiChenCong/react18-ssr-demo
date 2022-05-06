6b8b930737227889a2fd811e2771da2275283584
chencong <chencong@dian.so>
2022-05-06 14:01:32 +0800

    no message

--- client/route/index.js
+++ client/route/index.js
@@ -1,28 +1,23 @@
-import React from 'react'
-import { Route, Routes, useRoutes } from 'react-router-dom'
-import Home, { getInitialProps } from '../page/home'
-import Login from '../page/login'
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import Home, { getInitialProps } from "../page/home";
+import Login from "../page/login";
 
 const config = [
   {
-    path: '/',
+    path: "/",
     element: <Home />,
     loadData: getInitialProps,
   },
   {
-    path: '/login',
-    element: <Login />
-  }
-]
+    path: "/login",
+    element: <Login />,
+  },
+];
 
 export default function Index() {
-  let element = useRoutes(config)
-  return element
-  // return (
-  //   <Routes>
-  //     <Route path='/' exact element={<Home />} ></Route>
-  //     <Route path='/login' exact element={<Login />}></Route>
-  //   </Routes>
-  // )
+  let element = useRoutes(config);
+  return element;
 }
-export { config as routeConfig }
+
+export { config as routeConfig };
